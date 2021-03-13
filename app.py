7f1e40a146075ff464b591002cd94354db401162
yperez0914 <71104698+yperez0914@users.noreply.github.com>
2021-03-12 20:54:56 -0500

    flask and app.py

--- app.py
+++ app.py
@@ -44,6 +44,25 @@ Table('income', metadata,
      Column('Keydemo_per',Float)
      )
 
+Table('top100', metadata,
+     Column('Index', Integer, primary_key = True),
+     Column('PostalCode',Integer),
+     Column('Median_Income',Float),
+     Column('Population',Float),
+     Column('Median_age',Float), 
+     Column('Keydemo_per',Float),
+     Column('Brewery_count',Float),
+     Column('Starbucks_count',Float),
+     Column('pop_per_brewery',Float), 
+     Column('pop_per_starbucks',Float),
+     Column('Brewery_rank',Float), 
+     Column('Income_rank',Float),
+     Column('Demo_rank',Float),
+     Column('Starbucks_rank',Float),
+     Column('combined_score',Float), 
+     Column('Total_rank',Float)    
+     )
+
 # reflect an existing database into a new model
 Base = automap_base(metadata=metadata)
 # reflect the tables
@@ -56,7 +75,7 @@ Base.prepare(engine, reflect=True)
 starbucks = Base.classes.starbucks
 income = Base.classes.income
 brewery = Base.classes.brewery
-
+top100= Base.classes.top100
 ##########################
 #Flask Setup
 ##########################
@@ -73,12 +92,11 @@ def index():
     return(
         f"Home Base!<br/>"
         f"Available routes:<br/>"
-        f"Breweries per Zip Code:  /api/v1.0/brew_zip<br/>"
-        f"Defining the Brewery Standard: /api/v1.0/stations<br/>"
-        f"Map: Recommended places' population:  /api/v1.0/tobs<br/>"
-        f"Map: Recommended places' median income  /api/v1.0/YYYY-MM-DD<start><br/>"
-        f"Map: Existing Starbucks for Recommended Places:  /api/v1.0/YYYY-MM-DD<start>/YYYY-MM-DD<end>"
-        f"Map: Existing Breweries for Recommended Places:  /api/v1.0/YYYY-MM-DD<start>/YYYY-MM-DD<end>"
+        f"Top 100 Zip Codes:  /api/v1.0/top_zips<br/>"
+        f"Breweries in Top 100 Zip Codes: /api/v1.0/breweries<br/>"
+        f"Demographics in Top 100 Zip Codes:  /api/v1.0/demo<br/>"
+        f"Existing Starbucks in Top 100 Zip Codes:  /api/v1.0/starbucks>"
+        # f"Map: Existing Breweries for Recommended Places:  /api/v1.0/YYYY-MM-DD<start>/YYYY-MM-DD<end>"
         )
 
 @app.route("/api/v1.0/brew_zip")
@@ -101,7 +119,113 @@ def brew_zip():
  
      #Return the JSON representation of your dictionary.
     return jsonify(all_results)
+# @app.route("/api/v1.0/top_zips")
+# def top_zip_list():
+#     session = Session(engine)
+#     top_zips = session.query(top100.PostalCode)
+#     top_zip_list = []
+#     for i in top_zips:
+#         top_zip_list.append(i[0])
+#     session.close()
+#     return jsonify(top_zip_list)
 
+@app.route("/api/v1.0/breweries")
+def breweries():
+    session = Session(engine)
+    top_zips = session.query(top100.PostalCode)
+    top_zip_list = []
+    for i in top_zips:
+        top_zip_list.append(i[0])
+    brew_coord =session.query(brewery.PostalCode,brewery.Name, brewery.Latitude, brewery.Longitude).\
+        order_by(top100.PostalCode.desc()).\
+        filter(brewery.PostalCode.in_(top_zip_list)).\
+        filter(brewery.PostalCode == top100.PostalCode).all()
+    session.close()
+    brew_geo = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    for i in brew_coord:
+        feature = {"type": "Feature",
+                "geometry":{
+                    "type": "Point",
+                    "coordinates":[i[3], i[2]]
+                },
+                "properties":{
+                    "zipcode":i[0],
+                    "name":i[1]
+                }
+                }
+        brew_geo["features"].append(feature)
+    return jsonify(brew_geo)
+
+@app.route("/api/v1.0/demo")
+def demographics():
+    session = Session(engine)
+    top_zips = session.query(top100.PostalCode)
+    top_zip_list = []
+    for i in top_zips:
+        top_zip_list.append(i[0])
+    top_full = session.query(brewery.PostalCode, top100.Brewery_count, brewery.Latitude, brewery.Longitude,top100.Median_Income,top100.Population,top100.pop_per_brewery,top100.pop_per_starbucks, top100.Keydemo_per).\
+        order_by(top100.PostalCode.desc()).\
+        group_by(top100.PostalCode).\
+        filter(brewery.PostalCode.in_(top_zip_list)).\
+        filter(brewery.PostalCode == top100.PostalCode).all()
+    session.close()
+    demo_geo = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    for i in top_full:
+        feature = {"type": "Feature",
+                "geometry":{
+                    "type": "Point",
+                    "coordinates":[i[3], i[2]]
+                },
+                "properties":{
+                    "zipcode":i[0],
+                    "brewery_count":i[1],
+                    "median_income":i[4],
+                    "population": i[5],
+                    "pop_per_brewery": i[6],
+                    "pop_per_starbucks": i[7],
+                    "keydemo_per":i[8]
+                    
+                }
+                }
+        demo_geo["features"].append(feature)
+    return jsonify(demo_geo)
+
+
+@app.route("/api/v1.0/starbucks")
+def starbucks():
+    session =Session(engine)
+    top_zips = session.query(top100.PostalCode)
+    top_zip_list = []
+    for i in top_zips:
+        top_zip_list.append(i[0])
+    starbucks_coord =session.query(starbucks.PostalCode, starbucks.Latitude, starbucks.Longitude).\
+    order_by(starbucks.PostalCode).\
+    filter(starbucks.PostalCode.in_(top_zip_list)).\
+    filter(starbucks.PostalCode == top100.PostalCode).all()
+    session.close()
+
+    sbucks_geo = {
+    "type": "FeatureCollection",
+     "features": []
+}
+for i in starbucks_coord:
+    feature = {"type": "Feature",
+              "geometry":{
+                  "type": "Point",
+                  "coordinates":[i[2], i[1]]
+              },
+              "properties":{
+                  "zipcode":i[0]
+              }
+              }
+    sbucks_geo["features"].append(feature)
+    return jsonify(sbucks_geo)
 # @app.route("/api/v1.0/Starbucks_standard")
 # def stations():
 #     #Return a JSON list of stations from the dataset.
